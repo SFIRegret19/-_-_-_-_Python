4bb35355eff53ecbdb30e5ace43280a86352859c
Sp1rit <126536582+SFIRegret19@users.noreply.github.com>
2024-07-10 12:47:05 +0700

    Add files via upload

---
+++ Строки и индексация строк.py
@@ -0,0 +1,6 @@
+example = 'Абракадабра'
+print(example[0]) #Answer: А
+print(example[-1]) #Answer: а
+print(example[6:]) #Answer: дабра
+print(example[::-1]) #Answer: арбадакарбА
+print(example[1::2]) #Answer: бааар
